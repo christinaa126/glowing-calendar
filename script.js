f90bc8b67a21c39c25ca0b9cf693393b16c597f6
Christina <christinaa126@gmail.com>
2022-04-11 20:34:21 -0700

    changed for loop

--- script.js
+++ script.js
@@ -8,24 +8,28 @@ $(document).ready(function () {
         var time = $(this).parent().attr("id");
         localStorage.setItem(time, text);
     })
-    function theTime() {
-        var currentTime = moment().hour();
-        $(".time-block").each(function () {
-            var hour = parseInt($(".time-block").attr("id").split("currentTime")[1]);
+   
 
-            if (hour < currentTime) {
-                $(".time-block").addClass("past");
-            }
-            else if (hour === currentTime) {
-                $(".time-block").addClass("present");
-            }
-            else {
-                $(".time-block").addClass("future");
-            }
-        })
+   
+    var currentTime= moment().hour();
+
+    var timeBlockEl = $('.time-block')
+
+    for (var i = 0; i< timeBlockEl.length; i++) {
+        
+        if (timeBlockEl[i].id.slice(-2) < currentTime){
+            timeBlockEl.addClass('past')
+        } else if (timeBlockEl[i].id.slice(-2) === currentTime){
+            timeBlockEl.addClass('present')
+        }
+         else {
+             timeBlockEl.addClass('future')
+         }
     }
+
+
     
-    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-09 .description").val(localStorage.getItem("hour-09"));
     $("#hour-10 .description").val(localStorage.getItem("hour-10"));
     $("#hour-11 .description").val(localStorage.getItem("hour-11"));
     $("#hour-12 .description").val(localStorage.getItem("hour-12"));
@@ -34,5 +38,5 @@ $(document).ready(function () {
     $("#hour-15 .description").val(localStorage.getItem("hour-15"));
     $("#hour-16 .description").val(localStorage.getItem("hour-16"));
     $("#hour-17 .description").val(localStorage.getItem("hour-17"));
-    theTime();
+   
 })
